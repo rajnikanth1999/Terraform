5789889593ec8a08d26c4686c9d86e20342294f3
Rajnikanth1999 <praveendba31@gmail.com>
2022-11-02 13:44:24 +0530

    Added changes

--- ntier-aws/ec2.tf
+++ ntier-aws/ec2.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "ntier_ec2" {
   associate_public_ip_address = aws_eip.eip
   key_name = aws_key_pair.ec2_key
   subnet_id = aws_subnet.ntier_subnets[0].id
-#   private_ip = 
+  instance_type = "t2.micro"
   vpc_security_group_ids = [
     aws_security_group.ntier_nsg.id
   ]
